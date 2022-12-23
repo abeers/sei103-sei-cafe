1bfe1f172f87849e1d7e2ce84ef4f29f0154a7d4
Alex Beers <alexbeers92@gmail.com>
2022-12-22 20:18:25 -0500

    Fix everything

--- server.js
+++ server.js
@@ -33,10 +33,59 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
-const port = process.env.PORT || 3001
+module.exports = app
 
-app.listen(port, function () {
-  console.log(`Express app running on port ${port}`)
-})
+// // Configure to use port 3001 instead of 3000 during
+// // development to avoid collision with React's dev server
+// const port = process.env.PORT || 3001
+
+// app.listen(port, function () {
+//   console.log(`Express app running on port ${port}`)
+// })
+
+// const createError = require('http-errors')
+// const express = require('express')
+// const path = require('path')
+// const cookieParser = require('cookie-parser')
+// const logger = require('morgan')
+// const methodOverride = require('method-override')
+
+
+// // // view engine setup
+// // app.set('views', path.join(__dirname, 'views'))
+// // app.set('view engine', 'ejs')
+
+
+// app.use(cookieParser())
+// app.use(methodOverride('_method'))
+
+// app.use(function (req, res, next) {
+//   res.locals.user = req.user
+//   next()
+// })
+
+// const isLoggedIn = require('./config/auth')
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use('/', indexRouter)
+// app.use('/servants', servantsRouter)
+// app.use('/', isLoggedIn, reviewsRouter)
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404))
+// })
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message
+//   res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+//   // render the error page
+//   res.status(err.status || 500)
+//   res.render('error')
+// })
+
+// module.exports = app
